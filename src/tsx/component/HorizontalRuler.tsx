c281c824a7357a6801bc4b2cf256974d171c6cc7
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2018-10-06 21:32:14 +0200

    Add buttons and horizontal rulers

---
+++ src/tsx/component/HorizontalRuler.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HorizontalRuler: React.SFC = () => (
+    <hr className="hr" />
+);
+
+export default HorizontalRuler;
--- src/tsx/view/Words.tsx
+++ src/tsx/view/Words.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import Button from '../component/Button';
+import HorizontalRuler from '../component/HorizontalRuler';
 
-const Words: React.SFC = () => (
-    <div className="grid__sidebar"/>
-);
+const Words: React.SFC = () => {
+    const addWord = () => {
+        console.log('Add Word');
+    };
+
+    return (
+        <div className="grid__sidebar words">
+            <div>
+                <Button text="Add Word" className="button--full-width" onClick={addWord} />
+            </div>
+            <HorizontalRuler/>
+            <div className="words__content"/>
+        </div>
+    );
+};
 
 export default Words;
