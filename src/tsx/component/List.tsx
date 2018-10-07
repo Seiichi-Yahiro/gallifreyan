20b46de301474f62c16a110a65b69b1157a045f9
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2018-10-07 14:37:22 +0200

    Add renderItem function to List

--- src/tsx/component/List.tsx
+++ src/tsx/component/List.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 interface IListProps {
     readonly items?: any[];
     readonly generateKey?: (item: any, index?: number) => string;
+    readonly renderItem?: (item: any, index?: number) => string | JSX.Element;
     readonly isHorizontal?: boolean;
     readonly className?: string;
     readonly childClassName?: string;
@@ -11,6 +12,7 @@ interface IListProps {
 const List: React.SFC<IListProps> = ({
                                          items = [],
                                          generateKey = (item: any, index: number) => index.toString(),
+                                         renderItem = (item: any, index: number) => item,
                                          isHorizontal = false,
                                          className = '',
                                          childClassName = '',
@@ -21,7 +23,8 @@ const List: React.SFC<IListProps> = ({
     return (
         <div className={`list list--${orientation} ${className}`}>
             {items.map((item: any, index: number) => (
-                <div key={generateKey(item, index)} className={`list__item list__item--${orientation} ${childClassName}`}>{item}</div>))}
+                <div key={generateKey(item, index)}
+                     className={`list__item list__item--${orientation} ${childClassName}`}>{renderItem(item, index)}</div>))}
 
             {React.Children.map(children, (child: any, index: number) =>
                 React.cloneElement(child, {
