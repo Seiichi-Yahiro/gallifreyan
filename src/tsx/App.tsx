6bceabdd778b98fa01afe7dd925b3be13329c36b
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2018-10-07 15:27:27 +0200

    Move state up, implement word adding

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,17 +1,47 @@
 import * as React from 'react';
 import SVG from './view/SVG';
 import Words from './view/Words';
+import {v4} from 'uuid';
 
-class App extends React.Component {
+export interface IWord {
+    readonly id: string;
+    text: string;
+}
+
+interface IAppState {
+    words: IWord[];
+}
+
+class App extends React.Component<{}, IAppState> {
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            words: []
+        };
+    }
 
     public render() {
+        const {addWord} = this;
+        const {words} = this.state;
+
         return (
             <div className="grid">
-                <Words/>
+                <Words words={words} addWord={addWord}/>
                 <SVG/>
             </div>
         );
     }
+
+    private addWord = (text: string) => {
+        const newWord: IWord = {
+            id: v4(),
+            text
+        };
+
+        this.setState((prevState: IAppState) => ({words: [...prevState.words, newWord]}));
+    };
 }
 
 export default App;
--- src/tsx/view/Words.tsx
+++ src/tsx/view/Words.tsx
@@ -2,40 +2,68 @@ import * as React from 'react';
 import Button from '../component/Button';
 import HorizontalRuler from '../component/HorizontalRuler';
 import List from '../component/List';
+import {IWord} from '../App';
+import {createRef} from 'react';
 
-interface IWordsState {
-    words: string[];
+interface IWordsProps {
+    readonly words: IWord[];
+    readonly addWord: (text: string) => void;
 }
 
-class Words extends React.Component<{}, IWordsState> {
+interface IWordsState {
+    newWord: string;
+}
 
-    constructor (props: {}) {
+class Words extends React.Component<IWordsProps, IWordsState> {
+
+    private inputRef = createRef<HTMLInputElement>();
+
+    constructor(props: IWordsProps) {
         super(props);
 
         this.state = {
-            words: []
+            newWord: ''
         };
     }
 
     public render() {
-        const {addWord} = this;
-        const {words} = this.state;
+        const {inputRef, onTextInputChange, onAddWordClick, getWordKey, renderWord} = this;
+        const {words} = this.props;
+        const {newWord} = this.state;
 
         return (
             <div className="grid__sidebar words">
                 <List isHorizontal={true}>
-                    <input type="text" className="text-input" placeholder="Word..."/>
-                    <Button text="Add Word" onClick={addWord} className="button--full-width" />
+                    <input ref={inputRef} type="text" className="text-input" placeholder="Word..." value={newWord}
+                           onChange={onTextInputChange}/>
+                    <Button text="Add Word" onClick={onAddWordClick} className="button--full-width"/>
                 </List>
                 <HorizontalRuler/>
-                <List items={words}/>
+                <List items={words} generateKey={getWordKey} renderItem={renderWord}/>
             </div>
         );
     }
 
-    private addWord = () => {
-        console.log('Add Word');
-    }
+    private onTextInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newWord = event.currentTarget.value;
+        this.setState(() => ({newWord}));
+    };
+
+    private onAddWordClick = () => {
+        this.props.addWord(this.state.newWord);
+        this.setState(() => ({newWord: ''}));
+
+        const input = this.inputRef.current;
+
+        if (input) {
+            input.focus();
+        }
+    };
+
+    private getWordKey = (word: IWord) => word.id;
+
+    private renderWord = (word: IWord) => word.text;
+
 }
 
 export default Words;
