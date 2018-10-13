20cdd936a538a76cc03100fc0f0cd814fbdc281e
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2018-10-13 23:51:44 +0200

    Add XIcon

---
+++ src/tsx/icon/XIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {CSSProperties} from 'react';
+
+const XIcon = () => {
+    const lineStyle: CSSProperties = {
+        stroke: '#000000',
+        strokeWidth: 1
+    };
+
+    return (
+        <svg height={20} width={20} style={{cursor: 'pointer'}}>
+            <g style={{transform: 'translate(50%, 50%) rotate(45deg)'}}>
+                <line x1={-10} y1={0} x2={10} y2={0} style={lineStyle}/>
+                <line x1={0} y1={10} x2={0} y2={-10} style={lineStyle}/>
+            </g>
+        </svg>
+    );
+};
+
+export default XIcon;
