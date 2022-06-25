c6114e8136f937a718d87b6ea403200d92441845
Julian Viechter <jviechter@gmail.com>
2022-06-25 16:06:40 +0200

    Show angle constraints in angle slider

--- src/tsx/sidebar/work/settings/AngleInput.tsx
+++ src/tsx/sidebar/work/settings/AngleInput.tsx
@@ -1,6 +1,7 @@
 import { InputLabel } from '@mui/material';
 import React from 'react';
 import { useAppDispatch } from '../../../hooks/useAppDispatch';
+import { useRedux } from '../../../hooks/useRedux';
 import { AppThunkAction } from '../../../state/AppState';
 import { UUID } from '../../../state/image/ImageTypes';
 import { Degree } from '../../../utils/LinearAlgebra';
@@ -15,7 +16,7 @@ interface AngleInputProps {
 
 const AngleInput: React.FunctionComponent<AngleInputProps> = ({ id, angle, updateAngle, disabled }) => {
     const dispatch = useAppDispatch();
-    //TODO const {minAngle, maxAngle} = useRedux(state => state.work.constraints[id]!.angle);
+    const angleConstraints = useRedux((state) => state.work.constraints[id]!.angle);
 
     const changeAngle = (angle: number) => {
         dispatch(updateAngle(id, angle));
@@ -24,7 +25,13 @@ const AngleInput: React.FunctionComponent<AngleInputProps> = ({ id, angle, updat
     return (
         <div className="sidebar-work-settings__angle">
             <InputLabel>Angle</InputLabel>
-            <CircularSlider radius={50} value={angle} onChange={changeAngle} disabled={disabled} />
+            <CircularSlider
+                radius={50}
+                value={angle}
+                onChange={changeAngle}
+                disabled={disabled}
+                constraints={angleConstraints}
+            />
         </div>
     );
 };
--- src/tsx/sidebar/work/settings/CircularSlider.tsx
+++ src/tsx/sidebar/work/settings/CircularSlider.tsx
@@ -1,8 +1,9 @@
 import { useTheme } from '@mui/material';
 import React, { useRef, useState } from 'react';
 import useEventListener from '../../../hooks/useEventListener';
+import { AngleConstraints } from '../../../state/work/WorkTypes';
 import { calculateAngle, centerOfDOMRect } from '../../../utils/DragAndDrop';
-import { Degree, Position } from '../../../utils/LinearAlgebra';
+import { Degree, Position, rotate, toRadian } from '../../../utils/LinearAlgebra';
 import { adjustAngle } from '../../../utils/TextTransforms';
 
 interface CircularSliderProps {
@@ -10,6 +11,7 @@ interface CircularSliderProps {
     value: Degree;
     onChange: (newAngle: Degree) => void;
     disabled?: boolean;
+    constraints?: AngleConstraints;
 }
 
 const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
@@ -17,6 +19,7 @@ const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
     value,
     onChange,
     disabled = false,
+    constraints,
 }) => {
     const theme = useTheme();
 
@@ -71,27 +74,50 @@ const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
         target
     );
 
+    const activeColor = theme.palette.primary.main;
+    const disabledColor = theme.palette.grey.A400;
+    const currentColor = disabled ? disabledColor : activeColor;
+    const cursor = disabled ? 'default' : 'pointer';
+    const pointerEvents = disabled ? 'none' : 'auto';
+    const opacity = 0.38;
+
     return (
         <div
             style={{
                 position: 'relative',
-                //width: size,
-                //height: size,
-                color: disabled ? theme.palette.grey.A400 : theme.palette.primary.main,
             }}
         >
             <svg width={size} height={size}>
                 <g style={{ transform: `translate(${circleCenterOffset}px, ${circleCenterOffset}px)` }}>
-                    <circle
-                        ref={circleRef}
-                        r={radius}
-                        cx={0}
-                        cy={0}
-                        strokeWidth={strokeWidth}
-                        stroke="currentColor"
-                        opacity={0.38}
-                        fill="none"
-                    />
+                    {!disabled && constraints && !(Math.abs(constraints.maxAngle - constraints.minAngle) >= 360) ? (
+                        <g>
+                            <path
+                                d={describeArc(0, 0, radius, constraints.minAngle, constraints.maxAngle)}
+                                stroke={currentColor}
+                                fill="none"
+                                opacity={opacity}
+                                strokeWidth={strokeWidth}
+                            />
+                            <path
+                                d={describeArc(0, 0, radius, constraints.maxAngle, constraints.minAngle)}
+                                stroke={disabledColor}
+                                fill="none"
+                                opacity={opacity}
+                                strokeWidth={strokeWidth}
+                            />
+                        </g>
+                    ) : (
+                        <circle
+                            ref={circleRef}
+                            r={radius}
+                            cx={0}
+                            cy={0}
+                            strokeWidth={strokeWidth}
+                            stroke={currentColor}
+                            opacity={opacity}
+                            fill="none"
+                        />
+                    )}
                     <circle
                         r={radius}
                         cx={0}
@@ -100,11 +126,10 @@ const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
                         stroke="transparent"
                         fill="none"
                         onClick={disabled ? undefined : updateAngle}
-                        style={{ cursor: disabled ? 'default' : 'pointer', pointerEvents: disabled ? 'none' : 'auto' }}
+                        style={{ cursor, pointerEvents }}
                     />
                 </g>
             </svg>
-
             <span
                 className="MuiSlider-thumb"
                 onMouseOver={disabled ? undefined : () => setHovered(true)}
@@ -120,9 +145,9 @@ const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
                     top: circleCenterOffset - knobCenterOffset,
                     borderRadius: '50%',
                     position: 'absolute',
-                    backgroundColor: 'currentColor',
-                    cursor: disabled ? 'default' : 'pointer',
-                    pointerEvents: disabled ? 'none' : 'auto',
+                    backgroundColor: currentColor,
+                    cursor,
+                    pointerEvents,
                     boxShadow:
                         isHovered || isDragging
                             ? `0px 0px 0px ${isDragging ? 14 : 8}px rgb(144 202 249 / 16%)`
@@ -144,6 +169,22 @@ const CircularSlider: React.FunctionComponent<CircularSliderProps> = ({
             </span>
         </div>
     );
+};
+
+const describeArc = (x: number, y: number, radius: number, minAngle: Degree, maxAngle: Degree): string => {
+    const zeroVec = { x, y: y + radius };
+
+    const start = rotate(zeroVec, -toRadian(minAngle));
+    const end = rotate(zeroVec, -toRadian(maxAngle));
+
+    const length = Math.abs(maxAngle - minAngle);
+
+    const isLargeArc = length > 180;
+    const largeArcFlag = minAngle < maxAngle ? isLargeArc : !isLargeArc;
+
+    const sweep = 0;
+
+    return ['M', start.x, start.y, 'A', radius, radius, 0, Number(largeArcFlag), sweep, end.x, end.y].join(' ');
 };
 
 export default CircularSlider;
