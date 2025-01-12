e9db2513bcb07ac56987ff267d098b82924c5f09
Julian Viechter <jviechter@gmail.com>
2025-01-12 19:58:01 +0100

    Setup react with vite and eslint

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    { ignores: ['build'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'react-compiler': reactCompiler,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: false,
+                },
+            ],
+            'no-console': 'warn',
+            eqeqeq: ['error', 'always'],
+            'react-compiler/react-compiler': 'error',
+        },
+    },
+    eslintConfigPrettier,
+);
---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig, type UserConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ command }): UserConfig => {
+    const commonConfig: UserConfig = {
+        plugins: [
+            tsconfigPaths(),
+            react({
+                babel: {
+                    plugins: ['babel-plugin-react-compiler'],
+                },
+            }),
+            checker({
+                typescript: true,
+                eslint: {
+                    lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+                    useFlatConfig: true,
+                },
+            }),
+        ],
+    };
+
+    if (command === 'serve') {
+        return {
+            ...commonConfig,
+            server: {
+                port: 3000,
+            },
+        };
+    } else {
+        // command === 'build'
+        return {
+            ...commonConfig,
+            build: {
+                outDir: './build',
+                rollupOptions: {
+                    output: {
+                        manualChunks: {
+                            react: ['react', 'react-dom'],
+                        },
+                    },
+                },
+            },
+        };
+    }
+});
