ae542034d7ceda25e194fea755ebcd6d6540df5a
Julian Viechter <jviechter@gmail.com>
2025-01-19 17:31:48 +0100

    Setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default defineConfig((configEnv) =>
+    mergeConfig(
+        viteConfig(configEnv),
+        defineConfig({
+            test: {
+                environment: 'jsdom',
+                dir: 'src/',
+                coverage: {
+                    provider: 'v8',
+                    reporter: ['html'],
+                    reportsDirectory: './reports/coverage',
+                    include: ['src/**'],
+                },
+            },
+        }),
+    ),
+);
