f9baf7b37bfe6b1f4d53223af952b310af30f04f
Julian Viechter <jviechter@gmail.com>
2025-01-19 17:36:47 +0100

    Setup mutation testing with stryker

---
+++ stryker.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+    packageManager: 'pnpm',
+    reporters: ['html', 'progress'],
+    testRunner: 'vitest',
+    vitest: {
+        configFile: 'vitest.config.ts',
+        dir: 'src',
+    },
+    checkers: ['typescript'],
+    tsconfigFile: 'tsconfig.json',
+    coverageAnalysis: 'perTest',
+    incremental: true,
+    ignoreStatic: true,
+    plugins: [
+        '@stryker-mutator/vitest-runner',
+        '@stryker-mutator/typescript-checker',
+    ],
+};
+export default config;
